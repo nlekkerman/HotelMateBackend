5a609bbfecd56d59f583b0e1c8b0efd806370056
nlekkerman <nlekkerman@gmail.com>
2025-05-25 10:17:23 +0100

    Finalize settings for Heroku deployment

--- HotelMateBackend/settings.py
+++ HotelMateBackend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Load .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
