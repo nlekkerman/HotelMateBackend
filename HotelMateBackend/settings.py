a1513aadf9d0dde7ada49cbe9e621355d608534a
nlekkerman <nlekkerman@gmail.com>
2025-07-08 18:23:51 +0100

    refactor: set DEBUG to True for development environment

--- HotelMateBackend/settings.py
+++ HotelMateBackend/settings.py
@@ -31,7 +31,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
