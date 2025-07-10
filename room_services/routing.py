1430ea243db35539c6c1931afb0de85b5ad6ff8c
nlekkerman <nlekkerman@gmail.com>
2025-07-10 16:23:17 +0100

    refractiored to first version of ws.

--- room_services/routing.py
+++ room_services/routing.py
@@ -1,10 +1,6 @@
-# your_app/routing.py
 from django.urls import re_path
-from .consumers import OrderStatusConsumer
+from . import consumers
 
 websocket_urlpatterns = [
-    re_path(
-      r"ws/orders/(?P<hotel_slug>[-\w]+)/(?P<order_id>\d+)/$",
-      OrderStatusConsumer.as_asgi()
-    ),
+    re_path(r"ws/orders/(?P<order_id>\d+)/$", consumers.OrderStatusConsumer.as_asgi()),
 ]
