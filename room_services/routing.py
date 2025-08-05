e22881f2c0cd0e5aed6166290c2f8f50738223f7
nlekkerman <nlekkerman@gmail.com>
2025-08-05 13:41:34 +0100

    Update OrderStatusConsumer to include hotel_slug in group name and adjust routing for WebSocket connection

--- room_services/routing.py
+++ room_services/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/orders/(?P<order_id>\d+)/$", consumers.OrderStatusConsumer.as_asgi()),
+    # Capture hotel_slug (letters, digits, dashes) and order_id (digits)
+    re_path(r"ws/orders/(?P<hotel_slug>[\w-]+)/(?P<order_id>\d+)/$", consumers.OrderStatusConsumer.as_asgi()),
 ]
