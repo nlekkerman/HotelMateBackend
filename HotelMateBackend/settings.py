1efccaf811ef14a9bc4f4951f690fd8dbadc5ba1
nlekkerman <nlekkerman@gmail.com>
2025-08-05 14:39:03 +0100

    Remove SSL configuration from CHANNEL_LAYERS in settings.py

--- HotelMateBackend/settings.py
+++ HotelMateBackend/settings.py
@@ -139,7 +139,6 @@ CHANNEL_LAYERS = {
         "CONFIG": {
             # pass the URL directly, not a dict with ssl or ssl_context
             "hosts": [REDIS_URL],
-            "ssl": True,
             "ssl_cert_reqs": None,
         },
     },
